7d60fb4ce82f70a17c928e6f15dc2737b9132938
fikriibrahimahid <fikriibrahim1999@gmail.com>
2022-10-05 10:23:02 +0700

    initial commit

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import "github.com/gofiber/fiber"
+
+type MongoInstance struct {
+	Client
+	Db
+}
+
+var mg MongoInstance
+
+const dbName = "fiber-hrms"
+const mongoURI = "mongodb://localhost:27017/" + dbName
+
+type Employee struct {
+	ID     string
+	Name   string
+	Salary float64
+	Age    float64
+}
+
+func Connect() error {
+
+}
+
+func main() {
+	app := fiber.New()
+}
